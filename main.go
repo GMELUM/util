3b3c4d9c416ec922a913d381d18eeea99def4c69
GMELUM <gmelum@vk.com>
2024-02-03 14:27:37 +0300

    update

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	r.ParseMultipartForm(100 << 20) // Максимальный размер файла: 10MB
+	r.ParseMultipartForm(100 << 20)
 	file, header, err := r.FormFile("file")
 	if err != nil {
 		fmt.Println("Error Retrieving the File")
@@ -62,7 +62,7 @@ func listFiles(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprint(w, 
+	fmt.Fprint(w,
 		`<script>
 			function deleteAndReload(fileName) {
 				if (confirm('Вы уверены, что хотите удалить файл ' + fileName + '?')) {
@@ -84,7 +84,7 @@ func listFiles(w http.ResponseWriter, r *http.Request) {
 		</script>`,
 	)
 
-	fmt.Fprintf(w, "<div style='display: flex;align-items: center;justify-content: flex-start;flex-direction: row;flex-wrap: wrap;'>")
+	fmt.Fprintf(w, "<div style='display: flex;align-items: center;justify-content: flex-start;flex-direction: row;flex-wrap: wrap;font-family: -apple-system, system-ui, Helvetica Neue, Roboto, sans-serif;'>")
 	for _, file := range files {
 		fileName := file.Name()
 		filePath := fmt.Sprintf("/download/%s", fileName)
@@ -93,13 +93,28 @@ func listFiles(w http.ResponseWriter, r *http.Request) {
 		fileSizeMB := float64(fileSizeBytes) / (1024 * 1024)
 		fileModTime := fileStat.ModTime().Format("02.01.2006 15:04")
 
-		fmt.Fprintf(w, "<div style='display: flex;padding: 2px 12px;background: #ebebeb;font-size: 14pt;font-weight: 600;border-radius: 12px;flex-direction: column;flex-wrap: nowrap;align-items: center;margin: 6px;border: 2px dashed #8d8d8d;'>")
-		fmt.Fprintf(w, "<p>%s</p>", fileName)
-		fmt.Fprintf(w, "<p style='margin: 6px; font-size: 11pt; width: 100px; text-align: end;'>%.2f MB</p>", fileSizeMB)
-		fmt.Fprintf(w, "<p style='font-size: 10pt;font-weight: 500;color: #858585;margin: 2px 0;width: 100px;text-align: end;'>%s</p>", fileModTime)
-		fmt.Fprintf(w, "<a href=\"%s\" download=\"%s\"><button style='width: 100px;height: 36px;margin: 12px  0 8px 0;border-radius: 12px;border: none;background: #878787;font-weight: 600;color: white;cursor: pointer;'>Скачать</button></a>", filePath, fileName)
-		
+		fmt.Fprintf(w, "<div style='display: flex;width: 200px;height: 369px;background: #e9e9e9;font-size: 14pt;font-weight: 600;border-radius: 12px;flex-direction: column;flex-wrap: nowrap;align-items: center;margin: 6px;overflow: hidden;'>")
+
+		fmt.Fprint(w, "<div style='width: 200px;height: 200px; background: #878787;'>")
+		fileExt := filepath.Ext(fileName)
+		if fileExt == ".jpg" || fileExt == ".png" || fileExt == ".jpeg" {
+			fmt.Fprintf(w, "<img src=\"%v\" style='width: 200px;height: 200px;object-fit: cover;'></img>", filePath)
+		}
+		fmt.Fprintf(w, "</div>")
+
+		fmt.Fprintf(w, "<div style='display: flex;width: 200px;padding: 12px;box-sizing: border-box;flex-direction: column;flex-wrap: nowrap;align-items: flex-end;'>")
+
+		fmt.Fprint(w, "<div style='display: flex;height: 20px;width: 100%;'>")
+		fmt.Fprintf(w, "<p style='flex: 1 1;height: 20px;font-size: 11pt;margin: 0;display: block;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;padding: 0 8px 0 0;'>%s</p>", fileName)
+		fmt.Fprintf(w, "<p style='font-size: 11pt;text-align: end;box-sizing: border-box;margin: 0;'>%.2f MB</p>", fileSizeMB)
+		fmt.Fprintf(w, "</div>")
+
+		fmt.Fprintf(w, "<p style='font-size: 10pt;font-weight: 500;color: #858585;margin: 2px 0;width: 169px;text-align: end;'>%s</p>", fileModTime)
+		fmt.Fprintf(w, "<a href=\"%s\" download=\"%s\"><button style='width: 169px;height: 36px;margin: 12px  0 8px 0;border-radius: 12px;border: none;background: #878787;font-weight: 600;color: white;cursor: pointer;'>Скачать</button></a>", filePath, fileName)
+
 		fmt.Fprintf(w, "<button onclick=\"deleteAndReload('%s')\" style='width: 80px;height: 28px;border-radius: 12px;background: #252525;cursor: pointer;color: white;font-weight: 600;border: none;margin: 0 0 12px 0;'>Удалить</button>", fileName)
+
+		fmt.Fprintf(w, "</div>")
 
 		fmt.Fprintf(w, "</div>")
 
@@ -114,30 +129,30 @@ func downloadFile(w http.ResponseWriter, r *http.Request) {
 }
 
 func deleteFile(w http.ResponseWriter, r *http.Request) {
-  fileName := r.URL.Path[len("/delete/"):]
+	fileName := r.URL.Path[len("/delete/"):]
 
-  filePath := fmt.Sprintf("./dump/%s", fileName)
-  err := os.Remove(filePath)
-  if err != nil {
-    http.Error(w, "Failed to delete file", http.StatusInternalServerError)
-    return
-  }
+	filePath := fmt.Sprintf("./dump/%s", fileName)
+	err := os.Remove(filePath)
+	if err != nil {
+		http.Error(w, "Failed to delete file", http.StatusInternalServerError)
+		return
+	}
 
-  fmt.Fprintf(w, "File %s has been deleted", fileName)
+	fmt.Fprintf(w, "File %s has been deleted", fileName)
 }
 
 func basicAuth(handler http.HandlerFunc, username, password string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-	 user, pass, ok := r.BasicAuth()
- 
-	 if !ok || user != username || pass != password {
-		w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
-		http.Error(w, "Unauthorized", http.StatusUnauthorized)
-		return
-	 }
-	 handler(w, r)
+		user, pass, ok := r.BasicAuth()
+
+		if !ok || user != username || pass != password {
+			w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		handler(w, r)
 	}
- }
+}
 
 func main() {
 
